1be33951a3c1ce704b899abebf5560460fcda6db
AlaCrity <wlchen1105@gmail.com>
2019-07-08 19:40:19 -0400

    fixed the function processEntry, it contained a ; instead of a {

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -19,47 +19,57 @@ database.ref().on("child_added", function(snapshot) {
   let firstTrainTime = snapshot.firstTrainTime;
   let frequency = snapshot.frequency;
 
-// add code to calculate train time 
-var time = moment();
-var firstTrainCalc = moment(firstTrainTime, 'HH:MM A').subtract(1, 'years');
-var diff = moment().diff(moment(firstTrainCalc), 'minutes');
-var left = diff % frequency;
-var minLeft = snapFreq - left;
-var arrivalTime = moment().add(minLeft , 'm').format('HH:MM: A');
+  // add code to calculate train time
+  var time = moment();
+  var firstTrainCalc = moment(firstTrainTime, "HH:MM A").subtract(1, "years");
+  var diff = moment().diff(moment(firstTrainCalc), "minutes");
+  var left = diff % frequency;
+  var minLeft = snapFreq - left;
+  var arrivalTime = moment()
+    .add(minLeft, "m")
+    .format("HH:MM: A");
 
-let newRow = $('<tr>');
-$(newRow).append(`<td>${trainName}</td>`);
-$(newRow).append(`<td>${destination}</td>`);
-$(newRow).append(`<td>${frequency}</td>`);
-$(newRow).append(`<td>${nextArrival}</td>`);
-$(newRow).append(`<td>${minutesAway}</td>`);
+  let newRow = $("<tr>");
+  $(newRow).append(`<td>${trainName}</td>`);
+  $(newRow).append(`<td>${destination}</td>`);
+  $(newRow).append(`<td>${frequency}</td>`);
+  $(newRow).append(`<td>${nextArrival}</td>`);
+  $(newRow).append(`<td>${minutesAway}</td>`);
 
-$('#data-goes-here').append(newRow);
+  $("#data-goes-here").append(newRow);
 });
 
-function processEntry();
-event.preventDefault();
+function processEntry() {
+  event.preventDefault();
 
-let trainName = $('#trainName').val().trim();
-let destination = $('#destination').val().trim();
-let firstTrainTime = $('#firstTrainTime').val().trim();
-let frequency = $('#frequency').val().trim();
+  let trainName = $("#trainName")
+    .val()
+    .trim();
+  let destination = $("#destination")
+    .val()
+    .trim();
+  let firstTrainTime = $("#firstTrainTime")
+    .val()
+    .trim();
+  let frequency = $("#frequency")
+    .val()
+    .trim();
 
-database.ref().push({
+  database.ref().push({
     trainName: trainName,
     destination: destination,
     firstTrainTime: firstTrainTime,
     frequency: frequency,
-    nextArrival: '',
-    minutesAway: ''
-});
+    nextArrival: "",
+    minutesAway: ""
+  });
+}
 
 clear();
 
 function clear() {
-    $('#trainName').val('');
-    $('#destination').val('');
-    $('#firstTrainTime').val('');
-    $('#frequency').val('');
-};
-
+  $("#trainName").val("");
+  $("#destination").val("");
+  $("#firstTrainTime").val("");
+  $("#frequency").val("");
+}
